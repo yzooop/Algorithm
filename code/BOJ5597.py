bc2bafca6f2df15d747882910840c83c7459becc
yzooop <qkrduswn0404@gmail.com>
2024-06-22 22:46:42 +0900

    .

--- code/BOJ5597.py
+++ code/BOJ5597.py
@@ -2,11 +2,11 @@ import sys
 
 data = []
 
-for _ in range(3):
+for _ in range(28):
     number = int(sys.stdin.readline().strip())
     data.append(number)
 
-arr = [i+1 for i in range(5)]
+arr = [i+1 for i in range(30)]
 
 for num in data:
     if (num in arr):
