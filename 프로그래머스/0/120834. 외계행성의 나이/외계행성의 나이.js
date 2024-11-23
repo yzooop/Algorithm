13bc2bd5a6dc570e6f540981d2ce8ca7c2a583ee
yzooop <163278410+yzooop@users.noreply.github.com>
2024-11-23 11:46:57 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -1,15 +1,9 @@
 function solution(age) {
     const planet_age = "abcdefghij"
-    const age_str = age.toString()
-    let ans = []
     
-    for (let i = 0; i < age_str.length; i++){
-        for (let j = 0; j < planet_age.length; j++){
-            if (age_str[i] == j){
-                ans.push(planet_age[j])
-            }
-        }
-    }
-    return ans.join("")
-    
+    return age
+        .toString()
+        .split("")
+        .map((idx) => planet_age[idx])
+        .join("")
 }
