c0098f92deba8587a083a0cf80c0bb0242c7963c
yzooop <163278410+yzooop@users.noreply.github.com>
2025-02-09 23:06:31 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 7.22 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -1,11 +1,4 @@
 function solution(arr, queries) {
-    let answer = []
-    
-    queries.forEach((q) => {
-        const slice = arr.slice(q[0], q[1] + 1)
-        const ans = slice.filter((item) => item > q[2])
-        const min_value = Math.min(...ans) === Infinity ? -1 : Math.min(...ans)
-        answer.push(min_value)    
-    })
-    return answer
+    var answer = [];
+    return answer;
 }
