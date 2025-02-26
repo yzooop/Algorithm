5a9f6e58eabf318e1a5884ae36aecac172c5c758
yzooop <163278410+yzooop@users.noreply.github.com>
2025-02-26 20:33:45 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,12 @@
+function solution(num) {
+    let count = 0;
+
+    while (num !== 1) {
+        if (count >= 500) return -1;
+
+        num = num % 2 === 0 ? num / 2 : num * 3 + 1;
+        count++;
+    }
+
+    return count;
+}
