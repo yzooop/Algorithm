2ad2fbaa2879448b7e5c12131dcc134fd05315ab
yzooop <163278410+yzooop@users.noreply.github.com>
2025-06-06 00:19:07 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.62 ms, Memory: 9.41 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -3,29 +3,29 @@ from collections import deque
 def solution(maps):
     n = len(maps)
     m = len(maps[0])
-    
-    return dfs(0, 0, n-1, m-1, n, m, maps)
-
-def dfs(sy, sx, ey, ex, n, m, maps):
-    q = deque()
     v = [[0] * m for _ in range(n)]
     
+    ans = bfs(0, 0, n-1, m-1, n, m, maps, v)
+    
+    if ans == 0: 
+        return -1
+    else: 
+        return ans
+
+def bfs(sy, sx, ey, ex, n, m, arr, v):
+    q = deque()
     q.append((sy, sx))
     v[sy][sx] = 1
     
     while q:
         cy, cx = q.popleft()
         
-        # 종료조건
-        if (cy, cx) == (ey, ex):
-            return v[ey][ex]
-            
-        
         for dy, dx in ((1, 0), (-1, 0), (0, 1), (0, -1)):
-            ny, nx = cy+dy, cx+dx
+            ny, nx = cy + dy, cx + dx
             
-            if 0<=ny<n and 0<=nx<m and v[ny][nx] == 0 and maps[ny][nx] == 1:
+            if 0<=ny<n and 0<=nx<m and arr[ny][nx] == 1 and v[ny][nx] == 0:
                 q.append((ny, nx))
                 v[ny][nx] = v[cy][cx] + 1
-    return -1
-        
+                
+    return v[n-1][m-1]
+    
